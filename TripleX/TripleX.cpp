8d9d6ee1320077f76129e7d89cbd5899a087d3e3
Aaron Watkins <ac.watkins80@gmail.com>
2021-05-21 07:44:21 -0400

    change vars to upper camel casing give better names

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -5,20 +5,21 @@ int main()
 {
   // tell the use whats happening
   std::cout << "You are Ana trying to break into the North Mountain Castle where Elsa is hiding..." << std::endl;
-  std::cout << "You need to get the numbers correct in order to continue..." << std::endl;
+  std::cout << "You need to get the numbers on the lock correct to continue..." << std::endl;
 
   /*
     declare some variables
     this is a multiline comment
   */
-  const int a = 4;
-  const int b = 3;
-  const int c = 2;
-  const int sum = a + b + c;
-  const int product = a * b * c;
+  const int CodeLockNum1 = 4;
+  const int CodeLockNum2 = 3;
+  const int CodeLockNum3 = 2;
+  const int CodeSum = CodeLockNum1 + CodeLockNum2 + CodeLockNum3;
+  const int CodeProduct = CodeLockNum1 * CodeLockNum2 * CodeLockNum3;
 
-  // tell the use what the sum and product are
-  std::cout << "the sum of a, b, and c is " << sum << std::endl;
-  std::cout << "the product of a, b, and c is " << product << std::endl;
+  // tell the user what the sum and product are
+  std::cout << std::endl << "There are 3 numbers on the lock. " << std::endl;
+  std::cout << "the sum of the 3 numbers is: " << CodeSum << std::endl;
+  std::cout << "the product of the 3 numbers is: " << CodeProduct << std::endl;
   return 0;
 }
