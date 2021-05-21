a6dc86efa4a99fc838b897a91a455fa3d9af9395
Aaron Watkins <ac.watkins80@gmail.com>
2021-05-20 20:53:26 -0400

    del hello worlld line add line appropriate for game

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
-  std::cout << "Hello, World!";
+  std::cout << "You are Ana trying to break into the North Mountain Castle where Elsa is hiding...";
+  std::cout << "You need to get the numbers correct in order to continue...";
   return 0;
 }
