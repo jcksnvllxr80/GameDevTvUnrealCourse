7622feeec63954c8516e62d84579d5ee245e01e2
Aaron Watkins <ac.watkins80@gmail.com>
2021-05-20 21:17:13 -0400

    add const on all vars

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -5,12 +5,12 @@ int main()
   std::cout << "You are Ana trying to break into the North Mountain Castle where Elsa is hiding..." << std::endl;
   std::cout << "You need to get the numbers correct in order to continue..." << std::endl;
 
-  int a = 4;
-  int b = 3;
-  int c = 2;
+  const int a = 4;
+  const int b = 3;
+  const int c = 2;
 
-  int sum = a + b + c;
-  int product = a * b * c;
+  const int sum = a + b + c;
+  const int product = a * b * c;
 
   std::cout << "the sum of a, b, and c is " << sum << std::endl;
   std::cout << "the product of a, b, and c is " << product << std::endl;
