081d7017d78d384529fc11f1e1ac10d63a9f8141
Aaron Watkins <ac.watkins80@gmail.com>
2021-05-20 21:00:22 -0400

    assign a variable and print it out

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -5,5 +5,9 @@ int main()
   std::cout << "You are Ana trying to break into the North Mountain Castle where Elsa is hiding...";
   std::cout << std::endl;
   std::cout << "You need to get the numbers correct in order to continue...";
+  std::cout << std::endl;
+
+  int a = 4;
+  std::cout << a;
   return 0;
 }
