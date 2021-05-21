2907cdbe87f8b4bb9e3568f7903fc76a8750d9b9
Aaron Watkins <ac.watkins80@gmail.com>
2021-05-20 21:10:48 -0400

    concatenate stuff to consolidate lines

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -2,23 +2,17 @@
 
 int main()
 {
-  std::cout << "You are Ana trying to break into the North Mountain Castle where Elsa is hiding...";
-  std::cout << std::endl;
-  std::cout << "You need to get the numbers correct in order to continue...";
-  std::cout << std::endl;
+  std::cout << "You are Ana trying to break into the North Mountain Castle where Elsa is hiding..." << std::endl;
+  std::cout << "You need to get the numbers correct in order to continue..." << std::endl;
 
   int a = 4;
   int b = 3;
   int c = 2;
 
   int sum = a + b + c;
-  std::cout << "the sum of a, b, and c is ";
-  std::cout << sum;
-  std::cout << std::endl;
+  std::cout << "the sum of a, b, and c is " << sum << std::endl;
 
   int product = a * b * c;
-  std::cout << "the product of a, b, and c is ";
-  std::cout << product;
-  std::cout << std::endl;
+  std::cout << "the product of a, b, and c is " << product << std::endl;
   return 0;
 }
