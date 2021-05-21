be03cbdc0e95e9fcb0a9d8bbcc3fd049b6a23db8
Aaron Watkins <ac.watkins80@gmail.com>
2021-05-21 17:23:56 -0400

    lots of refactoring

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 
-// main function
-int main()
-{
-  // tell the use whats happening
-  std::cout << "You are Ana trying to break into the North Mountain Castle where Elsa is hiding..." << std::endl;
-  std::cout << "You need to get the numbers on the lock correct to continue..." << std::endl;
+void PrintIntroduction(int Level)
+{ // tell the use whats happening
+  std::cout << "Level: " << Level << "\n";
+  std::cout << "You are Ana trying to break into the North Mountain Castle where Elsa is hiding...\n";
+  std::cout << "You need to get the numbers on the lock correct to continue...\n";
+}
 
+
+auto GenerateCodeNumbers()
+{
+  struct retVals
+  { // Declare a local structure
+    int sum, product;
+  };
   /*
     declare some variables
     this is a multiline comment
@@ -14,34 +21,72 @@ int main()
   const int CodeLockNum1 = 4;
   const int CodeLockNum2 = 3;
   const int CodeLockNum3 = 2;
-  const int CodeSum = CodeLockNum1 + CodeLockNum2 + CodeLockNum3;
-  const int CodeProduct = CodeLockNum1 * CodeLockNum2 * CodeLockNum3;
 
-  // tell the user what the sum and product are
-  std::cout << std::endl << "There are 3 numbers on the lock. " << std::endl;
-  std::cout << "the sum of the 3 numbers is: " << CodeSum << std::endl;
-  std::cout << "the product of the 3 numbers is: " << CodeProduct << std::endl;
-  std::cout << "Please enter the code..." << std::endl;
+  return retVals{CodeLockNum1 + CodeLockNum2 + CodeLockNum3, CodeLockNum1 * CodeLockNum2 * CodeLockNum3};
+}
 
+
+void PrintHints(int CodeSum, int CodeProduct)
+{ // tell the user what the sum and product are
+  std::cout << "\n- There are 3 numbers on the lock. \n";
+  std::cout << "- The sum of the 3 numbers is: " << CodeSum;
+  std::cout << "\n- The product of the 3 numbers is: " << CodeProduct << std::endl;
+}
+
+
+bool TakeInputAndCompare(int CodeSum, int CodeProduct)
+{ // take players input
+  bool Winner;
   int PlayerGuess1, PlayerGuess2, PlayerGuess3;
-  std::cout << "Enter the first number..." << std::endl;
-  std::cin >> PlayerGuess1;
-  std::cout << "Enter the second number..." << std::endl;
-  std::cin >> PlayerGuess2;
-  std::cout << "Enter the third number..." << std::endl;
-  std::cin >> PlayerGuess3;
+  std::cout << "Please enter the numbers, separated by a space...\n";
+  std::cin >> PlayerGuess1 >> PlayerGuess2 >> PlayerGuess3;
 
   int PlayerGuessSum = PlayerGuess1 + PlayerGuess2 + PlayerGuess3;
   int PlayerGuessProduct = PlayerGuess1 * PlayerGuess2 * PlayerGuess3;
 
-  if (PlayerGuessSum == CodeSum & CodeProduct == PlayerGuessProduct)
+  // check if players numbers are correct
+  if (PlayerGuessSum == CodeSum & PlayerGuessProduct == CodeProduct)
   {
-    std::cout << "You got into the North Mountain Castle and found Elsa!!!" << std::endl;
+    Winner = true;
+    std::cout << "You got into the North Mountain Castle and found Elsa!!!\n";
   }
   else
   {
-    std::cout << "You did not get in. Please try again." << std::endl;
+    Winner = false;
+    std::cout << "You did not get in to the North Mountain Castle. Please try again.\n";
   }
+  return Winner;
+}
 
+
+void PlayLevel(int Level)
+{
+  bool PlayerWon = false;
+  PrintIntroduction(Level);
+  auto [retval1, retval2] = GenerateCodeNumbers();
+  const int CodeSum = retval1;
+  const int CodeProduct = retval2;
+  while (!PlayerWon)
+  {
+    PrintHints(CodeSum, CodeProduct);
+    PlayerWon = TakeInputAndCompare(CodeSum, CodeProduct);
+  }
+}
+
+
+int PlayGame()
+{
+  for (int Level = 0; Level <= 10; Level++)
+  {
+    PlayLevel(Level);
+  }
+  std::cout << "You finished all the levels! Good job!\n";
+}
+
+
+// main function
+int main()
+{
+  PlayGame();
   return 0;
 }
