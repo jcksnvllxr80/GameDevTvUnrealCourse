b1f782e38e18abe9b20936a1a319d43cddbb3de0
Aaron Watkins <ac.watkins80@gmail.com>
2021-05-20 21:22:11 -0400

    add some comments

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 
+// main function
 int main()
 {
+  // tell the use whats happening
   std::cout << "You are Ana trying to break into the North Mountain Castle where Elsa is hiding..." << std::endl;
   std::cout << "You need to get the numbers correct in order to continue..." << std::endl;
 
+  // declare som e variables
   const int a = 4;
   const int b = 3;
   const int c = 2;
-
   const int sum = a + b + c;
   const int product = a * b * c;
 
+  // tell the use what the sum and product are
   std::cout << "the sum of a, b, and c is " << sum << std::endl;
   std::cout << "the product of a, b, and c is " << product << std::endl;
   return 0;
