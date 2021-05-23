dfde9b60731b37c7a65c326d2852cb1129f908b6
Aaron Watkins <ac.watkins80@gmail.com>
2021-05-23 14:49:04 -0400

    add game loop flowchart add some text to game clear screen on begin game

--- BullsAndCows/Source/BullCowGame/BullCowCartridge.cpp
+++ BullsAndCows/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,12 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Mooooo! Welcome to the bull & cow game!"));
+    PrintLine(TEXT("Use TAB to access the terminal and then enter some text and press ENTER!"));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    FString HiddenWord = TEXT("uncopyrightable");
+    ClearScreen();
 }
