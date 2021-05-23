cc7a568e89b2bf2e0710e7708b683ddee917775f
Aaron Watkins <ac.watkins80@gmail.com>
2021-05-23 15:28:21 -0400

    add if statement

--- BullsAndCows/Source/BullCowGame/BullCowCartridge.cpp
+++ BullsAndCows/Source/BullCowGame/BullCowCartridge.cpp
@@ -5,11 +5,22 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Mooooo! Welcome to the bull & cow game!"));
+    PrintLine(TEXT("Guess the 16 letter isogram..."));  // Magic Number Remove
     PrintLine(TEXT("Use TAB to access the terminal and then enter some text and press ENTER!"));
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString& Input)  // When the player hits enter
 {
-    FString HiddenWord = TEXT("uncopyrightable");
+    FString HiddenWord = TEXT("uncopyrightable");  // move out side of this fundtion
     ClearScreen();
+    PrintLine(TEXT("You entered: ") + Input);
+
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("Correct! You win!"));
+    }
+    else
+    {
+        PrintLine(TEXT("Wrong! You lose!"));
+    }
 }
