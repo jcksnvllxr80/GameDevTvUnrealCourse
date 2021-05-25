4c8572a448a8275d1b43be3654c8f7183ff0a41b
Aaron Watkins <ac.watkins80@gmail.com>
2021-05-24 23:29:03 -0400

    update flowchart make HiddenWord an instance.memeber variable

--- BullsAndCows/Source/BullCowGame/BullCowCartridge.cpp
+++ BullsAndCows/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,6 +4,7 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    HiddenWord = TEXT("uncopyrightable");
     PrintLine(TEXT("Mooooo! Welcome to the bull & cow game!"));
     PrintLine(TEXT("Guess the 16 letter isogram..."));  // Magic Number Remove
     PrintLine(TEXT("Use TAB to access the terminal and then enter some text and press ENTER!"));
@@ -11,7 +12,6 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 
 void UBullCowCartridge::OnInput(const FString& Input)  // When the player hits enter
 {
-    FString HiddenWord = TEXT("uncopyrightable");  // move out side of this fundtion
     ClearScreen();
     PrintLine(TEXT("You entered: ") + Input);
 
