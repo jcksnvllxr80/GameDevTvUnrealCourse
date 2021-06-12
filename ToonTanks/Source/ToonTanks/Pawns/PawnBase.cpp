3b9498a0a238fe61ca7214c2f5247542f95c70c0
Aaron Watkins <ac.watkins80@gmail.com>
2021-06-12 13:57:26 -0400

    add PawnBase class c++

--- ToonTanks/Source/ToonTanks/Pawns/PawnBase.cpp
+++ ToonTanks/Source/ToonTanks/Pawns/PawnBase.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Pawns/PawnBase.h"
+#include "PawnBase.h"
 
 // Sets default values
 APawnBase::APawnBase()
