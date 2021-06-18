21b6bf3e1fd98d02f5bbc6748a89f4c29cd88644
Aaron Watkins <ac.watkins80@gmail.com>
2021-06-17 21:05:36 -0400

    add pawnturret class

---
+++ ToonTanks/Source/ToonTanks/Pawns/PawnTurret.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PawnTurret.h"
+
+// Called when the game starts or when spawned
+void PawnTurret::BeginPlay()
+{
+  Super::BeginPlay();
+}
+
+// Called every frame
+void PawnTurret::Tick(float DeltaTime)
+{
+  Super::Tick(DeltaTime);
+
+}
