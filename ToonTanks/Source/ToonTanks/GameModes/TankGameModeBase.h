1dfa610425cbbec626fb2e19c0634400ea83b2fe
Aaron Watkins <ac.watkins80@gmail.com>
2021-06-21 19:50:29 -0400

    add gamemode blueprint add gamemode c++ class

---
+++ ToonTanks/Source/ToonTanks/GameModes/TankGameModeBase.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API ATankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+
+private:
+
+	void HandleGameStart();
+	void HandleGameOver(bool PlayerWon);
+
+public:
+	void ActorDied(AActor* DeadActor);
+
+protected:
+	virtual void BeginPlay() override;
+	UFUNCTION(BlueprintIMplementableEvent)
+	void GameStart();
+	UFUNCTION(BlueprintIMplementableEvent)
+	void GameOver(bool PlayerWon);
+};
