4594ee9338d374d32b03fe994cce91c945249494
Aaron Watkins <ac.watkins80@gmail.com>
2021-07-04 14:23:55 -0400

    set player location in enemy AI black board memory implement behavior tree to move AI between player location and its own start location as an exercise

--- SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
+++ SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
@@ -3,6 +3,7 @@
 
 #include "ShooterAIController.h"
 #include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
  void AShooterAIController::BeginPlay()
 {
@@ -11,6 +12,8 @@
 	 if (AIBehavior)
 	 {
 		 RunBehaviorTree(AIBehavior);
+		 GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+		 GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
 	 }
 }
 
@@ -18,16 +21,17 @@ void AShooterAIController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 	// always move to actor with an acceptance radius of 200cm (2 meters)
-	if (PlayerPawn && LineOfSightTo(PlayerPawn))
-	{
-		SetFocus(PlayerPawn);
-		MoveToActor(PlayerPawn, AcceptanceRadius);
-	}
-	else
-	{
-		ClearFocus(EAIFocusPriority::Gameplay);
-		StopMovement();
-	}
+	
+	//if (PlayerPawn && LineOfSightTo(PlayerPawn))
+	//{
+	//	SetFocus(PlayerPawn);
+	//	MoveToActor(PlayerPawn, AcceptanceRadius);
+	//}
+	//else
+	//{
+	//	ClearFocus(EAIFocusPriority::Gameplay);
+	//	StopMovement();
+	//}
 	
 
 }
