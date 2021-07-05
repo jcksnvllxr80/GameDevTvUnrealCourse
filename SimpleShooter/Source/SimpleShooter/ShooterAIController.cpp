fa6ba7e895d4e75765a81841e95693bd46c847fa
Aaron Watkins <ac.watkins80@gmail.com>
2021-07-05 13:33:16 -0400

    modify behavior tree to be more intelligent add custom black board node called clear BB value

--- SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
+++ SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
@@ -12,7 +12,6 @@
 	 if (AIBehavior)
 	 {
 		 RunBehaviorTree(AIBehavior);
-		 GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
 		 GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
 	 }
 }
@@ -22,16 +21,17 @@ void AShooterAIController::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 	// always move to actor with an acceptance radius of 200cm (2 meters)
 	
-	//if (PlayerPawn && LineOfSightTo(PlayerPawn))
-	//{
-	//	SetFocus(PlayerPawn);
-	//	MoveToActor(PlayerPawn, AcceptanceRadius);
-	//}
-	//else
-	//{
-	//	ClearFocus(EAIFocusPriority::Gameplay);
-	//	StopMovement();
-	//}
-	
+	if (PlayerPawn)
+	{
+		if (LineOfSightTo(PlayerPawn))
+		{
+			GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+			GetBlackboardComponent()->SetValueAsVector(TEXT("LastKnownPlayerLocation"), GetBlackboardComponent()->GetValueAsVector(TEXT("PlayerLocation")));
+		}
+		else
+		{
+			GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
+		}
+	}
 
 }
