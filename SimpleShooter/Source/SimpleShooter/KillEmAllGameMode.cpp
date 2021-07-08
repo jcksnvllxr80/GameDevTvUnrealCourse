47ad102a404c66affb31bc9fd083629cced77da3
Aaron Watkins <ac.watkins80@gmail.com>
2021-07-08 19:51:29 -0400

    add endgame function in the killemall game mode

--- SimpleShooter/Source/SimpleShooter/KillEmAllGameMode.cpp
+++ SimpleShooter/Source/SimpleShooter/KillEmAllGameMode.cpp
@@ -2,6 +2,8 @@
 
 
 #include "KillEmAllGameMode.h"
+#include "EngineUtils.h"
+#include "GameFramework/Controller.h"
 
 void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
 {
@@ -10,6 +12,16 @@ void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
 	if (PlayerController)
 	{
 		UE_LOG(LogTemp, Display, TEXT("The Pawn, %s, was killed."), *PawnKilled->GetName())
-		PlayerController->GameHasEnded(nullptr, false);
+		EndGame(false);
+	}
+}
+
+void AKillEmAllGameMode::EndGame(bool bIsPlayerWinner)
+{
+	for (AController* Controller : TActorRange<AController>(GetWorld()))
+	{
+		bool bIsWinner = Controller->IsPlayerController() == bIsPlayerWinner;
+		Controller->GameHasEnded(Controller->GetPawn(), bIsWinner);
+
 	}
 }
